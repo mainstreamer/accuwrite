73e80e0d971f59514f19c6cdd4b9d4ed3401a298
Artem Dolmatov <artem.kyiv@outlook.com>
2017-04-18 01:44:53 +0300

    alternative suggestions selection

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -8,7 +8,8 @@ function goCheck(text) {
         }
     }).done(function(data) {
         $( this ).addClass( "done" );
-        alert('success');
+        console.log('ajax successful');
+        console.log(data);
         var f =  JSON.parse(data);
         suggest(f);
     }).fail(
@@ -22,8 +23,65 @@ function goCheck(text) {
 function suggest(array) {
 
     for (var name in array) {
-        // console.log('name : '+ name+' value : '+array[name]);
-        console.log($(":contains('fas')" ));
+        // console.log('id : '+ name+' suggestion : '+array[name]['value']);
+        console.log($('#word_'+name).title = array[name]['value']);
+        buildTooltip(name, array);
+
+
+
+        // console.log($(":contains('fas')" ));
     }
+}
+
+
+function buildTooltip(name, array) {
+
+    $('#word_'+name).tooltip({trigger:'manual',
+        title : array[name]['value'],
+        // container : '#word_'+name
+    }).tooltip('show');
+    $('#word_'+name).after(' ');
+
+
+    // $('#word_'+name).next()[0].append("<div class='tooltip-inner'>LAL</div>");
+    // $('div.tooltip-inner:not(.cross)').append('<div class="tooltip-inner cross reject" pid="'+name+'"><i class="fa fa-times"></i></div>');
+
+    // $('div.tooltip-inner:not(.cross)').append('<div class="tooltip-inner approve cross"><i class="fa fa-check"></i></div>');
+
+    // $('div.reject').on('click', function (){
+    //     $('div.reject').closest('.tooltip').hide();
+    // });
+
+    $('.tooltip').on('click', function(el){
+        $(this).prev().text(el.target.innerText);
+        $(this).hide();
+    });
+
+
+    $('span').on('click', function(el){
+        if ($(this).next().hasClass('tooltip')) {
+            $(this).next().hide();
+        }
+        // text(el.target.innerText);
+        // $(this).hide();
+    });
+
+}
+
+function approveSuggestion() {
+
+    $('div.tooltip-inner.approve').on('click', function (){
+        var newVal = $('div.tooltip-inner.approve').parent('.tooltip-inner')[0].innerText;
+        $('#word_'+name).text(newVal);
+        $('div.tooltip-inner.approve').closest('.tooltip').hide();
+        // $('div.tooltip').hide();
+    });
+}
+
+function rejectSuggestion() {
+    $('div.tooltip-inner.reject').on('click', function (){
+        $('div.tooltip-inner.reject').closest('.tooltip').hide();
+    });
+
 }
 
--- spellchecker.php
+++ spellchecker.php
@@ -3,5 +3,12 @@
 // $input = 'fas afs a asf as f';
 
 
-echo json_encode(['fas' => 'fast']);
-//echo json_encode(explode(" ",$_POST['text'])); exit;
+//echo json_encode(['fas' => 'fast']);
+//echo json_encode(explode(" ",$_POST['text']));
+unset($_POST['text']);
+
+$_POST['text'][1] = ['id' => 1, 'value' => 'First'];
+//$_POST['text'][2] = ['id' => 2, 'value' => 'Second'];
+$_POST['text'][5] = ['id' => 5, 'value' => 'five'];
+
+echo json_encode($_POST['text']);
